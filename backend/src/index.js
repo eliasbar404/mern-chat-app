894b44e1f0edcb17daf15f47c3187f4673586e81
ilyas <ibaraaouche@gmail.com>
2024-05-20 08:44:01 +0100

    create message and user controllers / create middleware / message and user routes / socket

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,34 @@
+import path from "path";
 import express  from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
+import { app, server } from "./socket/socket.js";
 
 const app = express()
 dotenv.config();
 
+const __dirname = path.resolve();
 
 const port = process.env.PORT || 3030;
 
 app.use(express.json())
-app.use("/api/auth", authRoutes);
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
 
 app.listen(port, () => {
     connectToMongoDB()
